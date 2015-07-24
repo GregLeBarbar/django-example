8d49198a762ff09d6b5ec7b15b870bde924e6a46
Charmier Grégory <gregory.charmier@epfl.ch>
2015-07-24 17:15:36 +0200

    test2

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -85,4 +85,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
-TEMPLATE_DIRS = os.path.join(BASE_DIR, 'templates')
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
